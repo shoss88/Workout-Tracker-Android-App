1c522f61d9d080b4a1b52a7332f13f935f793ca6
Sahir Hossain <sahirh88@gmail.com>
2023-06-16 19:20:35 -0400

    added routine adding functionality

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.workouttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private int routineCount = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createRoutine(View v){
+        Button newRoutine = new Button(MainActivity.this);
+        newRoutine.setText((CharSequence)("Routine " + routineCount));
+        this.routineCount++;
+        LinearLayout routineList = (LinearLayout)findViewById(R.id.routineList);
+        routineList.addView(newRoutine);
     }
 }
