b83ac4a449cadd5ef14c34ea299283b146edda09
Sahir Hossain <sahirh88@gmail.com>
2023-06-18 12:32:31 -0400

    added clear all routines functionality

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -1,27 +1,34 @@
 package com.example.workouttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     private int routineCount = 1;
+    LinearLayout routineList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        routineList = (LinearLayout)findViewById(R.id.routineList);
     }
 
-    public void createRoutine(View v){
+    public void addRoutine(View v){
         Button newRoutine = new Button(MainActivity.this);
         newRoutine.setText((CharSequence)("Routine " + routineCount));
-        this.routineCount++;
-        LinearLayout routineList = (LinearLayout)findViewById(R.id.routineList);
+        routineCount++;
         routineList.addView(newRoutine);
+    }
+    public void clearRoutines(View v){
+        routineList.removeAllViews();
+        routineCount = 1;
     }
 }
