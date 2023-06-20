b2d4788ae1b96a393bc492019041a5826f5884b1
Sahir Hossain <sahirh88@gmail.com>
2023-06-20 17:26:36 -0400

    added dialog boxes for editing routines

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.workouttracker;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     int routineCount = 1;
@@ -27,14 +28,66 @@ public class MainActivity extends AppCompatActivity {
 
     public void addRoutine(View v){
         LayoutInflater li = getLayoutInflater();
-        View routineBox = li.inflate(R.layout.routine_box, routineList, false);
-        TextView routineName = (TextView) routineBox.findViewById(R.id.routineName);
+        View routineBox = li.inflate(R.layout.general_box, routineList, false);
+        TextView routineName = (TextView) routineBox.findViewById(R.id.boxName);
+        ImageButton routineEdit = (ImageButton) routineBox.findViewById(R.id.boxEdit);
+        ImageButton routineDelete = (ImageButton) routineBox.findViewById(R.id.boxDelete);
+
         routineName.setText((CharSequence) ("Routine " + routineCount));
-        routineCount++;
+        routineName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        routineEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editRoutineNameDialog(v);
+            }
+        });
+        routineDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         routineList.addView(routineBox);
+        routineCount++;
     }
     public void clearRoutines(View v){
         routineList.removeAllViews();
         routineCount = 1;
+    }
+    public void editRoutineNameDialog(View v){
+        TextView routineName = ((RelativeLayout)v.getParent()).findViewById(R.id.boxName);
+        Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.edit_routine_name_dialog);
+        Button submit = dialog.findViewById(R.id.ERSubmitButton);
+        Button cancel = dialog.findViewById(R.id.ERCancelButton);
+        TextView textBox = ((TextView)dialog.findViewById(R.id.editTextBox));
+        textBox.setText(routineName.getText());
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String givenName = textBox.getText().toString();
+                if (givenName.length() > 19){
+                    TextView submitInfo = dialog.findViewById(R.id.submitInfo);
+                    submitInfo.setTextColor(Color.parseColor("#FF0000"));
+                    submitInfo.setText((CharSequence) "Too many characters: 19 characters max");
+                }
+                else{
+                    routineName.setText((CharSequence) givenName);
+                    dialog.dismiss();
+                }
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
